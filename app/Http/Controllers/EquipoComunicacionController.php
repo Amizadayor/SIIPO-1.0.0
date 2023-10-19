1c27866edbf8552410c4f3eef9479c46ab7985d9
Amizadayor <Juanamizadayo@gmail.com>
2023-10-19 14:36:07 -0600

    Controlador-Modelo-Migración 0.0.32

---
+++ app/Http/Controllers/EquipoComunicacionController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\EquipoComunicacion;
+use Illuminate\Http\Request;
+
+class EquipoComunicacionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(EquipoComunicacion $equipoComunicacion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, EquipoComunicacion $equipoComunicacion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(EquipoComunicacion $equipoComunicacion)
+    {
+        //
+    }
+}
---
+++ app/Models/EquipoComunicacion.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EquipoComunicacion extends Model
+{
+    use HasFactory;
+    protected $table = 'equipos_comunicacion';
+
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'NombreEquipoComunicacion',
+    ];
+
+    protected $timestamps = true;
+}
