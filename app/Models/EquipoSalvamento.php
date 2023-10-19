a673d9b0a76c340f0af99bafe9de4ebdc1f05020
Amizadayor <Juanamizadayo@gmail.com>
2023-10-19 13:12:41 -0600

    Controlador-Modelo-Migración 0.0.30

---
+++ app/Models/EquipoSalvamento.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EquipoSalvamento extends Model
+{
+    use HasFactory;
+    protected $table = 'equipos_salvamento';
+
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'NombreEquipoSalvamento',
+    ];
+    protected $timestamps = true;
+}
