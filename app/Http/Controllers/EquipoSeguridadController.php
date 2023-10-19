5da8632966423aaf74cf07276c85e69eb44069bc
Amizadayor <Juanamizadayo@gmail.com>
2023-10-19 13:05:15 -0600

    Controlador-Modelo-Migración 0.0.29

---
+++ app/Http/Controllers/EquipoSeguridadController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\EquipoSeguridad;
+use Illuminate\Http\Request;
+
+class EquipoSeguridadController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(EquipoSeguridad $equipoSeguridad)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, EquipoSeguridad $equipoSeguridad)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(EquipoSeguridad $equipoSeguridad)
+    {
+        //
+    }
+}
---
+++ app/Models/EquipoSeguridad.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EquipoSeguridad extends Model
+{
+    use HasFactory;
+    protected $table = 'equipos_seguridad';
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'NombreEquipoSeguridad',
+    ];
+
+    protected $timestamps = true;
+}
