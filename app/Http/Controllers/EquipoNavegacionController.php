18e6d45a6e9b6794b7e0b60ad78043abaa133700
Amizadayor <Juanamizadayo@gmail.com>
2023-10-19 14:39:25 -0600

    Controlador-Modelo-Migración 0.0.33

---
+++ app/Http/Controllers/EquipoNavegacionController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\EquipoNavegacion;
+use Illuminate\Http\Request;
+
+class EquipoNavegacionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(EquipoNavegacion $equipoNavegacion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, EquipoNavegacion $equipoNavegacion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(EquipoNavegacion $equipoNavegacion)
+    {
+        //
+    }
+}
---
+++ app/Models/EquipoNavegacion.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EquipoNavegacion extends Model
+{
+    use HasFactory;
+    protected $table = 'equipos_comunicacion';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'NombreEquipoComunicacion',
+    ];
+
+    protected $timestamps = true;
+}
