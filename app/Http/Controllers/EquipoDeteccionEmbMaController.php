8d63ce9a826064051ecc4d0f4bd500e26fc9bff0
Amizadayor <Juanamizadayo@gmail.com>
2023-10-20 03:42:22 -0600

    Controlador-Modelo-Migración 0.0.36

---
+++ app/Http/Controllers/EquipoDeteccionEmbMaController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\EquipoDeteccionEmbMa;
+use Illuminate\Http\Request;
+
+class EquipoDeteccionEmbMaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(EquipoDeteccionEmbMa $equipoDeteccionEmbMa)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, EquipoDeteccionEmbMa $equipoDeteccionEmbMa)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(EquipoDeteccionEmbMa $equipoDeteccionEmbMa)
+    {
+        //
+    }
+}
---
+++ app/Models/EquipoDeteccionEmbMa.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\EquipoDeteccion;
+use App\Models\DatosGeneralesEmbMa;
+
+class EquipoDeteccionEmbMa extends Model
+{
+    use HasFactory;
+    protected $table = 'equipos_deteccion_emb_ma';
+
+    protected $fillable = [
+        'EqpoDeteccionid',
+        'DGEMMAid',
+    ];
+
+    public $timestamps = true;
+
+    public function EquipoDeteccion()
+    {
+        return $this->belongsTo(EquipoDeteccion::class, 'EqpoDeteccionid', 'id');
+    }
+
+    public function DatosGeneralesEmbMa()
+    {
+        return $this->belongsTo(DatosGeneralesEmbMa::class, 'DGEMMAid', 'id');
+    }
+}
