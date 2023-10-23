b6bddd883cc66f74063f96c57cdb51644ecf3595
Amizadayor <Juanamizadayo@gmail.com>
2023-10-23 06:54:29 -0600

    Controlador-Modelo-Migración 0.0.42

---
+++ app/Http/Controllers/EquipoComunicacionEmbMaController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\EquipoComunicacionEmbMa;
+use Illuminate\Http\Request;
+
+class EquipoComunicacionEmbMaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(EquipoComunicacionEmbMa $equipoComunicacionEmbMa)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, EquipoComunicacionEmbMa $equipoComunicacionEmbMa)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(EquipoComunicacionEmbMa $equipoComunicacionEmbMa)
+    {
+        //
+    }
+}
---
+++ app/Models/EquipoComunicacionEmbMa.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\EquipoComunicacion;
+use App\Models\DatosGeneralesEmbMa;
+
+class EquipoComunicacionEmbMa extends Model
+{
+    use HasFactory;
+    protected $table = 'equipos_comunicacion_emb_ma';
+
+    protected $fillable = [
+        'EqpoComunicacionid',
+        'DGEMMAid',
+    ];
+
+    public function EquipoComunicacion()
+    {
+        return $this->belongsTo(EquipoComunicacion::class, 'EqpoComunicacionid', 'id');
+    }
+
+    public function DatosGeneralesEmbMa()
+    {
+        return $this->belongsTo(DatosGeneralesEmbMa::class, 'DGEMMAid', 'id');
+    }
+}
