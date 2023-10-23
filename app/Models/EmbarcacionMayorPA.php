57edb5a3826c20d943b353bf318b3bc3c79d2359
Amizadayor <Juanamizadayo@gmail.com>
2023-10-22 21:02:45 -0600

    Controlador-Modelo-Migración 0.0.23

--- app/Models/EmbarcacionMayorPA.php
+++ app/Models/EmbarcacionMayorPA.php
@@ -5,28 +5,24 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\DatosGeneralesPA;
-use App\Models\DatosGeneralesEmbMA;
+use App\Models\DatosGeneralesEmbMa;
 
 class EmbarcacionMayorPA extends Model
 {
     use HasFactory;
     protected $table = 'embarcacionesmayores_pa';
     protected $primaryKey = 'id';
-
     protected $fillable = [
         'DGPAid',
         'DGEMMAid',
     ];
-
     protected $timestamps = true;
-
     public function DatosGeneralesPA()
     {
         return $this->belongsTo(DatosGeneralesPA::class, 'DGPAid', 'id');
     }
-
-    public function DatosGeneralesEmbMA()
+    public function DatosGeneralesEmbMa()
     {
-        return $this->belongsTo(DatosGeneralesEmbMA::class, 'DGEMMAid', 'id');
+        return $this->belongsTo(DatosGeneralesEmbMa::class, 'DGEMMAid', 'id');
     }
 }
