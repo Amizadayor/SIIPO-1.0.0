8ea35a33a4deff5488e5993b8b348af0bd3ccc24
Amizadayor <Juanamizadayo@gmail.com>
2023-10-23 15:16:28 -0600

    Controlador-Modelo-Migración 0.0.69

---
+++ app/Http/Controllers/TipoModalidadController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TipoModalidad;
+use Illuminate\Http\Request;
+
+class TipoModalidadController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TipoModalidad $tipoModalidad)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, TipoModalidad $tipoModalidad)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TipoModalidad $tipoModalidad)
+    {
+        //
+    }
+}
